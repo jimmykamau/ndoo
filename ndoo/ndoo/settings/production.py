090c3ecb5da8e988a4094e76ca898a542f449ded
Jimmy Kamau <jimmy.kamau@andela.com>
2016-12-05 11:57:25 +0300

    [Feature #133654631] Deploy app to Heroku

--- ndoo/ndoo/settings/production.py
+++ ndoo/ndoo/settings/production.py
@@ -3,17 +3,13 @@ Production specific settings for ndoo project.
 """
 
 from .base import *
+import dj_database_url
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.getenv('DB_PATH'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': 'localhost',
-        'PORT': '',
-    }
+    'default': dj_database_url.config()
 }
+
+ALLOWED_HOSTS = ['*']
